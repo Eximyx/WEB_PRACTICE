fc2182e4854e83a285ad74947a54f8eade658b64
Eximyx <mr.vitalik.12352@gmail.com>
2024-06-17 15:42:27 +0300

    save

--- js/script.js
+++ js/script.js
@@ -50,62 +50,235 @@ new Swiper('#swiper-prices', {
     },
 });
 
+new Swiper('#swiper-markups', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1.2,
+            spaceBetween: 1
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 1.5,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        1100: {
+            slidesPerView: 2,
+            spaceBetween: 5
+        },
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 5
+        },
+        1300: {
+            slidesPerView: 3,
+            spaceBetween: 5
+        },
+        1400: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        }
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+
+new Swiper('#swiper-reviews', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1.2,
+            spaceBetween: 1
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 1,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        1100: {
+            slidesPerView: 2,
+            spaceBetween: 5
+        },
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 5
+        },
+        1300: {
+            slidesPerView: 3,
+            spaceBetween: 5
+        },
+        1400: {
+            slidesPerView: 2,
+            spaceBetween: 5
+        }
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+
+new Swiper('#swiper-youtube', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1.2,
+            spaceBetween: 1
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 1,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        1100: {
+            slidesPerView: 2,
+            spaceBetween: 5
+        },
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 5
+        },
+        1300: {
+            slidesPerView: 3,
+            spaceBetween: 5
+        },
+        1400: {
+            slidesPerView: 3,
+            spaceBetween: 5
+        }
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+
 
 $('document').ready(() => {
 
-    var workSwipersAmount = $('.workSwiper').length;
+    $('.workSwiper').each(
+        (key, value) => {
+            console.log()
+            var thumb_swiper = $(value).parents()[0].getElementsByClassName('thumbs')[0];
 
-    for (let i = 1; i < workSwipersAmount; i++) {
-        new Swiper('#swiper-works-' + i, {
-            // Optional parameters
-            direction: 'vertical',
-            loop: true,
+            thumb_swiper = new Swiper(thumb_swiper, {
+                spaceBetween: 10,
+                slidesPerView: 4,
+                loop: true,
+                freeMode: true,
+                loopedSlides: 5, //looped slides should be the same
+                watchSlidesVisibility: true,
+                watchSlidesProgress: true,
+            });
 
-            // If we need pagination
-            pagination: {
-                el: '.swiper-pagination',
-            },
+            new Swiper(value, {
+                // Optional parameters
+                direction: 'vertical',
+                loop: true,
 
-            // Navigation arrows
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
+                // If we need pagination
+                pagination: {
+                    el: '.swiper-pagination',
+                },
 
-            breakpoints: {
-                // when window width is >= 320px
-                320: {
-                    slidesPerView: 1.2,
-                    spaceBetween: 1
+                // Navigation arrows
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
                 },
-                // when window width is >= 480px
-                480: {
-                    slidesPerView: 1.5,
-                    spaceBetween: 30
-                },
-                // when window width is >= 640px
-                1100: {
-                    slidesPerView: 2,
-                    spaceBetween: 5
-                },
-                1200: {
-                    slidesPerView: 3,
-                    spaceBetween: 5
-                },
-                1300: {
-                    slidesPerView: 3,
-                    spaceBetween: 5
-                },
-                1400: {
-                    slidesPerView: 4,
-                    spaceBetween: 5
-                }
-            },
 
-            // And if we need scrollbar
-            scrollbar: {
-                el: '.swiper-scrollbar',
-            },
-        });
-    }
+                breakpoints: {
+                    // when window width is >= 320px
+                    320: {
+                        slidesPerView: 1,
+                        spaceBetween: 1
+                    },
+                    // when window width is >= 480px
+                    480: {
+                        slidesPerView: 1.5,
+                        spaceBetween: 30
+                    },
+                    // when window width is >= 640px
+                    1100: {
+                        slidesPerView: 2,
+                        spaceBetween: 5
+                    },
+                    1200: {
+                        slidesPerView: 3,
+                        spaceBetween: 5
+                    },
+                    1300: {
+                        slidesPerView: 3,
+                        spaceBetween: 5
+                    },
+                    1400: {
+                        slidesPerView: 1,
+                        spaceBetween: 5
+                    }
+                }, thumbs: {
+                    swiper: thumb_swiper
+                },
+
+                // And if we need scrollbar
+                scrollbar: {
+                    el: '.swiper-scrollbar',
+                },
+            });
+        }
+    );
 });
+
+
